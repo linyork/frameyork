ce34433f6657ecfa0c417d7f9320b9580db95027
York <jason945119@gmail.com>
2018-05-08 12:29:42 +0800

    fix home a at header

--- Site/frameyork.tw/Application/Home.php
+++ Site/frameyork.tw/Application/Home.php
@@ -6,6 +6,7 @@ class Home extends \Template\AMaster
 {
     public function index()
     {
+        $this->addCss('/assets/css/components.min.css');
         $this->setTitle('首頁');
         $this->display('/home.phtml');
     }

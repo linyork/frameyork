25fa4e604f8a7c33455a7bb4f668b7b758d03c09
York <jason945119@gmail.com>
2018-05-08 14:06:45 +0800

    add home

--- Site/frameyork.tw/Application/Home.php
+++ Site/frameyork.tw/Application/Home.php
@@ -6,8 +6,28 @@ class Home extends \Template\AMaster
 {
     public function index()
     {
+        $array = array();
+        $dirname = ROOT_PATH . "/assets/md/";
+        $dh = \opendir( $dirname );
+        while( $dave = \readdir( $dh ) )
+        {
+            if( $dave != "." && $dave != ".." )
+            {
+                $file = \explode("-", $dave);
+                if( isset( $array[$file[0]]) )
+                {
+                    \array_push($array[$file[0]], $file[1] );
+                }
+                else
+                {
+                    $array[$file[0]] = array();
+                    \array_push($array[$file[0]], $file[1] );
+                }
+            }
+        }
+        \closedir( $dh );
         $this->addCss('/assets/css/components.min.css');
         $this->setTitle('首頁');
-        $this->display('/home.phtml');
+        $this->display('/home.phtml',array( "md_list" => $array ));
     }
 }
